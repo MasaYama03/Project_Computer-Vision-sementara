4eab2cef6e4b5a3c1d14d6f63f2c9e5753546706
MasaYama03 <masahiroymzk24@gmail.com>
2025-08-27 01:43:08 +0700

    refactor: Remove host and port from app.run

--- BE/app.py
+++ BE/app.py
@@ -1864,4 +1864,4 @@ def log_response(response):
 
 if __name__ == '__main__':
     # Run without debug mode to prevent reloader issues with YOLO model
-    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
+    app.run(debug=False, use_reloader=False)
